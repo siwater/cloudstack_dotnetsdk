2071f82d4c54d1baa33ebc0c2c75afa3de63f7b7
Simon Waterhouse <simon.waterhouse@citrix.com>
2013-04-25 16:26:20 +0100

    Update version number

--- CloudStack.SDK/Properties/AssemblyInfo.cs
+++ CloudStack.SDK/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription(".NET client side library for CLoudStack API")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("2.02.0.0")]
-[assembly: AssemblyFileVersion("2.02.0.0")]
+[assembly: AssemblyVersion("2.02.0.5")]
+[assembly: AssemblyFileVersion("2.02.0.5")]
 [assembly: AssemblyCompany("The Apache Software Foundation")]
 [assembly: AssemblyCopyright("Copyright © The Apache Software Foundation 2013")]
 
